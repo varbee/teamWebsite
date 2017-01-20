794364e51c493d20990fb24392fadb611c6b3526
1486019507@qq.com <1486019507@qq.com>
2017-01-20 16:57:12 +0800

    更新代码

--- assets/tpl/content.tpl
+++ assets/tpl/content.tpl
@@ -1,5 +1,9 @@
-
 <div class="cy_banner"></div>
 <div class="container"></div>
 <div class="hxq_contact"></div>
+<div class="num" javascript>
+	<span>1</span>
+	<span>2</span>
+	<span>3</span>
+</div>
 
--- components/cy_banner.js
+++ components/cy_banner.js
@@ -6,4 +6,60 @@ import tpl from "../assets/tpl/cy_banner.tpl";
 
 $('.cy_banner').append(tpl());
 
+var w=$('.slider li').width();
+var l=0;
+var timer=null;
+var len=$(".slider li").length*2;
+$(".slider").append($(".slider").html()).css({'width':len*w,'left':-len*w/2});
+function init(){
+	$(".banner .next").trigger('click');
+}
+timer=setInterval(init,2000);
+$(".slider li").hover(function(){
+	clearInterval(timer);
 
+},function(){
+	timer=setInterval(init,2000);
+});
+
+	  $(".prev").click(function(){
+
+		  l=parseInt($(".slider").css("left"))+w; 
+
+			 showCurrent(l); 
+
+		  });
+
+		  $(".next").click(function(){
+
+		     l=parseInt($(".slider").css("left"))-w;  
+
+			showCurrent(l);
+
+	  });
+
+	   function showCurrent(l){    
+
+	   if($(".slider").is(':animated')){ 
+
+	      return;
+
+	   }
+
+		  $(".slider").animate({"left":l},500,function(){
+
+				if(l==0){ 
+
+			   $(".slider").css("left",-len*w/2);   
+
+			   
+
+			 }else if(l==(1-len)*w){ 
+
+				 $(".slider").css('left',(1-len/2)*w); 
+
+				}
+
+		    }); 	  
+
+		 }
