1b74ff1cf78584d3fcc21c3f82e99e8c5c88c8a5
rounddream <pmy000@foxmail.com>
2017-02-16 16:36:58 +0800

    中间模块遮罩

--- components/container.js
+++ components/container.js
@@ -166,29 +166,10 @@ $(function () {
     	})
     }();
 
-    $('#black').click(function () {
-    		$('#black').hide();
-    		console.log("df");
-    });
-    
-    $("#bl-icon").each(function(index, element){
-		var animation = TweenMax.to(this, 0.2, {
-	    	className: '+= superShadow',
-	    	marginTop: '-10px',
-	    	marginBottom: '10px',
-	    	ease: Power1.easeIn,
-	    	paused:true
-	  	});
-	  	element.animation = animation;
-	})
+    $('.light h2,.flare').click(function () {
+    	$('#black').fadeOut();
+    })
 
-
-	$('#bl-icon').hover(function(){
-	 	this.animation.play()
-		}, 
-		function(){
-	 		this.animation.reverse();
-	})
 
 
 });
