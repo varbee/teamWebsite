d97e7e19997371381b234858329e0d38fdbb8682
1486019507@qq.com <1486019507@qq.com>
2017-02-17 20:23:53 +0800

    添加产品模块

---
+++ assets/tpl/nav.tpl
@@ -0,0 +1,4 @@
+<div class="nav-menu">
+	<img src="../assets/images/logo.png" alt="logo">
+	<span>勤奋蜂团队</span>
+</div>
--- components/honor.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import "../assets/css/honor.scss";
-import tpl from "../assets/tpl/honor.tpl";
-
-$('.footer').append(tpl());
